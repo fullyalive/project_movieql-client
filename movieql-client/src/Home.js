ff184f124d08ca6714be3386fd3755426f7c77b1
fullyalive <wogud1202@gmail.com>
2018-09-26 13:41:19 +0900

    Getting data from the GraphQL API

--- movieql-client/src/Home.js
+++ movieql-client/src/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Query from "react-apollo"; // react-apollo는 리액트에 연결하는 방법
+import { HOME_PAGE } from "./queries";
 
-const Home = () => <div>Home</div>;
+const Home = () => <Query query={HOME_PAGE}>{}</Query>;
 
 export default Home;
---
+++ movieql-client/src/queries.js
@@ -0,0 +1,11 @@
+import gql from "graphql-tag"; // 프론트엔드에 graphql을 작성하는법
+
+export const HOME_PAGE = gql`
+  query {
+    movies(limit: 50, rating: 8.5) {
+      id
+      title
+      genres
+    }
+  }
+`;
