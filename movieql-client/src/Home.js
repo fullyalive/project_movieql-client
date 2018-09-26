e7bb4f4a82c9ef15f99e4636e9a9ab32e2430783
fullyalive <wogud1202@gmail.com>
2018-09-26 13:55:18 +0900

    Getting data from the GraphQL API

--- movieql-client/src/Home.js
+++ movieql-client/src/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
-import Query from "react-apollo"; // react-apollo는 리액트에 연결하는 방법
+import { Query } from "react-apollo"; // react-apollo는 리액트에 연결하는 방법
 import { HOME_PAGE } from "./queries";
 
-const Home = () => <Query query={HOME_PAGE}>{}</Query>;
+const Home = () => (
+  <Query query={HOME_PAGE}>
+    {({ loading, data, error }) => {
+      if (loading) return <span>loading</span>;
+      if (error) return <span>something happened</span>;
+      if (data) {
+        return data.movies.map(movie => (
+          <h2 key={movie.id}>
+            {movie.title} / {movie.rating}
+          </h2>
+        ));
+      }
+    }}
+  </Query>
+);
 
 export default Home;
